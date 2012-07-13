daaf9578e2ce39bf9592ee1f6c221b7969f563b8
Ruben de Vries <ruben@rubensayshi.com>
2012-07-13 17:23:40 +0200

    added comment to config example

--- config.inc.example.php
+++ config.inc.example.php
@@ -1,4 +1,8 @@
 <?php
 
+# copy and rename to config.inc.php
+# and fill in the values
+# config.inc.php is on .gitignore so it won't be placed in version control!
+
 define('EMAIL',    'test@example.com');
 define('PASSWORD', 'p@ssw0rd');
