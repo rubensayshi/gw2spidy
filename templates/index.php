147ed5204795bd5eab959a03f297b4e1f546bf56
Ruben de Vries <ruben@rubensayshi.com>
2012-07-25 09:32:16 +0200

    try

--- templates/index.php
+++ templates/index.php
@@ -9,17 +9,19 @@
 ?>
     <div class="well">
         <a href="/index.php?act=type&type=<?php echo $type->getId() ?>"><h4><?php echo $type->getTitle() ?></h4></a>
-        <ul class="nav nav-pills nav-stacked">
-            <?php foreach ($type->getSubTypes() as $subtype):
-                    if ($subtype->getTitle()):
-            ?>
-                <li>
-                    <a href="/index.php?act=type&type=<?php echo $type->getId() ?>&subtype=<?php echo $subtype->getId() ?>"><?php echo $subtype->getTitle() ?></a>
-                </li>
-            <?php
-                    endif;
-                endforeach; ?>
-        </ul>
+        <?php if ($type->getSubTypes()->count()) : ?>
+            <ul class="nav nav-pills nav-stacked">
+                <?php foreach ($type->getSubTypes() as $subtype):
+                        if ($subtype->getTitle()):
+                ?>
+                    <li>
+                        <a href="/index.php?act=type&type=<?php echo $type->getId() ?>&subtype=<?php echo $subtype->getId() ?>"><?php echo $subtype->getTitle() ?></a>
+                    </li>
+                <?php
+                        endif;
+                    endforeach; ?>
+            </ul>
+        <?php endif; ?>
     </div>
 <?php
         endif;
