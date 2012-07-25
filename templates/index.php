75f6d5f7a8f5eb14dd8b34b55e95f4de953f6862
Ruben de Vries <ruben@rubensayshi.com>
2012-07-25 09:27:16 +0200

    try

--- templates/index.php
+++ templates/index.php
@@ -5,14 +5,24 @@
 
 ?>
 <ul class="nav nav-pills nav-stacked">
-    <?php foreach ($types as $type): ?>
+    <?php foreach ($types as $type):
+            if ($type->getTitle()):
+    ?>
         <li>
             <a href="/index.php?act=type&type=<?php echo $type->getId() ?>"><?php echo $type->getTitle() ?></a>
+            <ul>
+                <?php foreach ($type->getSubTypes() as $subtype):
+                        if ($subtype->getTitle()):
+                ?>
+                    <li>
+                        <a href="/index.php?act=type&type=<?php echo $type->getId() ?>&subtype=<?php echo $subtype->getId() ?>"><?php echo $subtype->getTitle() ?></a>
+                    </li>
+                <?php
+                        endif;
+                    endforeach; ?>
+            </ul>
         </li>
-        <?php foreach ($type->getSubTypes() as $subtype): ?>
-            <li>
-                <a href="/index.php?act=type&type=<?php echo $type->getId() ?>&subtype=<?php echo $subtype->getId() ?>"><?php echo $subtype->getTitle() ?></a>
-            </li>
-        <?php endforeach; ?>
-    <?php endforeach; ?>
+    <?php
+            endif;
+        endforeach; ?>
 </ul>
