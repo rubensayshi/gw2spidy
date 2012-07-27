faa5056dbfb566ce9e5b540ffd4ae2f685fbd661
Ruben de Vries <ruben@rubensayshi.com>
2012-07-27 12:11:44 +0200

    added memcache pooling for subtype and type too

--- src/GW2Spidy/DB/ItemTypePeer.php
+++ src/GW2Spidy/DB/ItemTypePeer.php
@@ -3,12 +3,12 @@
 namespace GW2Spidy\DB;
 
 use GW2Spidy\DB\om\BaseItemTypePeer;
-
+use \Propel;
 
 /**
  * Skeleton subclass for performing query and update operations on the 'item_type' table.
  *
- * 
+ *
  *
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
@@ -17,5 +17,76 @@ use GW2Spidy\DB\om\BaseItemTypePeer;
  * @package    propel.generator.gw2spidy
  */
 class ItemTypePeer extends BaseItemTypePeer {
+    protected static $memPool = null;
 
-} // ItemTypePeer
+    /**
+     * @return MemcacheInstancePool
+     */
+    protected static function getMemPool() {
+        if (is_null(self::$memPool)) {
+            self::$memPool = MemcacheInstancePool::getInstance(self::DATABASE_NAME . "::" . self::TABLE_NAME);
+        }
+
+        return self::$memPool;
+    }
+
+    public static function addInstanceToPool($obj, $key = null) {
+        if (Propel::isInstancePoolingEnabled()) {
+            if ($key === null) {
+                $key = (string) $obj->getDataId();
+            } // if key === null
+
+            if ($memPool = self::getMemPool()) {
+                $memPool->addInstanceToPool($obj, $key);
+            }
+
+            ItemPeer::$instances[$key] = $obj;
+        }
+    }
+
+    public static function removeInstanceFromPool($value)
+    {
+        if (Propel::isInstancePoolingEnabled() && $value !== null) {
+            if (is_object($value) && $value instanceof Item) {
+                $key = (string) $value->getDataId();
+            } elseif (is_scalar($value)) {
+                // assume we've been passed a primary key
+                $key = (string) $value;
+            } else {
+                $e = new PropelException("Invalid value passed to removeInstanceFromPool().  Expected primary key or Item object; got " . (is_object($value) ? get_class($value) . ' object.' : var_export($value,true)));
+                throw $e;
+            }
+
+            if ($memPool = self::getMemPool()) {
+                $memPool->removeInstanceFromPool($key);
+            }
+
+            unset(ItemPeer::$instances[$key]);
+        }
+    }
+
+    public static function getInstanceFromPool($key)
+    {
+        if (Propel::isInstancePoolingEnabled()) {
+            if (isset(ItemPeer::$instances[$key])) {
+                return ItemPeer::$instances[$key];
+            }
+
+            if ($memPool = self::getMemPool()) {
+                return $memPool->getInstanceFromPool($key);
+            }
+        }
+
+        return null; // just to be explicit
+    }
+
+    public static function clearInstancePool()
+    {
+        if ($memPool = self::getMemPool()) {
+            $memPool->clearInstancePool();
+        }
+
+        ItemPeer::$instances = array();
+    }
+
+}
