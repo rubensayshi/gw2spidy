b34ba517bea269dd220f86abb37291898c0a5521
Ruben de Vries <ruben@rubensayshi.com>
2012-08-01 10:42:48 +0200

    dev

--- templates/item.php
+++ templates/item.php
@@ -9,22 +9,38 @@ use GW2Spidy\Application;
 <?php echo Application::getInstance()->render("item_snippet", array('item' => $item, 'tag' => 'div')) ?>
 
 <div id="placeholder" style="width: 950px; height: 400px;"></div>
+<button class="btn" id="clear-selection" />
 
 <script type="text/javascript">
 $.ajax("/index.php?act=chart&id=<?php echo $item->getDataId() ?>", {
-    success: function(chart) {
-        chart = $.parseJSON(chart);
-
-        var plot = $.plot($("#placeholder"), chart, {
+    success: function(chartdata) {
+        var chartdata   = $.parseJSON(chartdata);
+        var placeholder = $("#placeholder");
+        var options     = {
+            series: {
+                lines:  { show: true },
+                points: { show: true }
+            },
             xaxis: {
                 mode:            "time",
                 timeformat:      "%y-%0m-%0d %H:%S",
                 twelveHourClock: false
-            }
+            },
+            selection: { mode: "x" }
+        };
+
+        placeholder.bind("plotselected", function (event, ranges) {
+            plot = $.plot(placeholder, chartdata,
+                          $.extend(true, {}, options, {
+                              xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to }
+                          }));
         });
 
-        plot.drawAxisDivs();
+        var plot = $.plot(placeholder, chartdata, options);
 
+        $("#clear-selection").click(function () {
+            plot.clearSelection();
+        });
     }
 });
 </script>
