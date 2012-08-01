4d88a8b844a7f6865c96982f45c419e5cff26fb8
Ruben de Vries <ruben@rubensayshi.com>
2012-08-01 12:33:16 +0200

    removed unused config

--- config/config.inc.example.php
+++ config/config.inc.example.php
@@ -8,12 +8,3 @@ define('LOGIN_EMAIL',    'test@example.com');
 define('LOGIN_PASSWORD', 'p@ssw0rd');
 
 define('AUTH_URL', 'https://account.guildwars2.com/login?redirect_uri=http://tradingpost-live.ncplatform.net/authenticate?source=%2F&game_code=gw2');
-
-/*
- * 650 requests with a 5min timeout
- *  gives us 650 x (60 / (300 / 60)) = 7800 requests / hr = 2.166 requests / sec
- *
- * in 1 hour we create about 6500 jobs, our total requests / hr should exceed this by at least 10% to be able to catch up
- */
-define('SLOTS',        650);
-define('SLOT_TIMEOUT', 300);
--- src/GW2Spidy/Queue/RequestSlotManager.php
+++ src/GW2Spidy/Queue/RequestSlotManager.php
@@ -8,7 +8,12 @@ class RequestSlotManager extends RedisSlotManager {
     protected function getSlotsQueueName() {
         return 'request.slots';
     }
-
+    /**
+     * 650 requests with a (300 sec = ) 5min timeout
+     *  gives us 650 x (60 / (300 / 60)) = 7800 requests / hr = 2.166 requests / sec
+     *
+     * in 1 hour we create about 6500 jobs, our total requests / hr should exceed this by at least 10% to be able to catch up
+     */
     protected function getSlots() {
         return 650;
     }
