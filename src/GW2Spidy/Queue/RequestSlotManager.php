a267a5524ed98ef3156045e52eb78c77a42d6508
Ruben de Vries <ruben@rubensayshi.com>
2012-08-03 16:01:21 +0200

    MOAR SLOTS

--- src/GW2Spidy/Queue/RequestSlotManager.php
+++ src/GW2Spidy/Queue/RequestSlotManager.php
@@ -9,17 +9,18 @@ class RequestSlotManager extends RedisSlotManager {
         return 'request.slots';
     }
     /**
-     * 650 requests with a (280 sec = ) 4.666 min timeout
-     *  gives us 650 x (60 / (280 / 60)) = 8357 requests / hr = 2.3 requests / sec
+     * 700 requests with a (250 sec = ) 4.666 min timeout
+     *  gives us 700 x (60 / (250 / 60)) = 10080 requests / hr = 2.8 requests / sec
+     * this is excluding the time it takes to handle the slots
      *
-     * in 1 hour we create about 6500 jobs, our total requests / hr should exceed this by at least 10% to be able to catch up
+     * in 1 hour we create about 6500 jobs, our total requests / hr should exceed this by at least 25% to be able to catch up
      */
     protected function getSlots() {
-        return 650;
+        return 700;
     }
 
     protected function getTimeout() {
-        return 280;
+        return 250;
     }
 
     /**
