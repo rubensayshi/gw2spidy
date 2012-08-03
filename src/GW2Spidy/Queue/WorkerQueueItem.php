a6551d54b4823f61d8af71e3b7c03a36e3b2a139
Ruben de Vries <ruben@rubensayshi.com>
2012-08-03 12:32:39 +0200

    readded priority constants eventhough we dont use them atm

--- src/GW2Spidy/Queue/WorkerQueueItem.php
+++ src/GW2Spidy/Queue/WorkerQueueItem.php
@@ -5,6 +5,15 @@ namespace GW2Spidy\Queue;
 use GW2Spidy\Util\RedisQueue\RedisQueueItem;
 
 class WorkerQueueItem extends RedisQueueItem {
+    const PRIORITY_EXTREME    = 9999;
+    const PRIORITY_HIGH       = 1000;
+    const PRIORITY_MED        = 500;
+    const PRIORITY_TYPEDB     = 499;
+    const PRIORITY_LISTINGSDB = 102;
+    const PRIORITY_ITEMDB     = 101;
+    const PRIORITY_LOW        = 100;
+    const PRIORITY_VERY_LOW   = 0;
+
     protected $worker;
     protected $priority;
 
