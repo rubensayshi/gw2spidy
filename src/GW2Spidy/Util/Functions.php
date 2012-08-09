454b0661ec1e08251c2edc3a02567a747847c0d4
Ruben de Vries <ruben@rubensayshi.com>
2012-08-09 10:11:57 +0200

    Functions::almostEqualCompare

--- src/GW2Spidy/Util/Functions.php
+++ src/GW2Spidy/Util/Functions.php
@@ -4,6 +4,10 @@ namespace GW2Spidy\Util;
 
 abstract class Functions {
     public static function almostEqualCompare($left, $right) {
+        if (trim(strtlower($left)) == trim(strtolower($right))) {
+            return 100;
+        }
+
         $p = 0;
         $i = similar_text($left, $right, $p);
 
