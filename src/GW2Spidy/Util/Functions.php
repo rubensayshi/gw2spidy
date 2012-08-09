d504b2ecb2584d0d13b339eef4f5800d37a3a6ac
Ruben de Vries <ruben@rubensayshi.com>
2012-08-09 09:59:36 +0200

    almostequal compare ;)

--- src/GW2Spidy/Util/Functions.php
+++ src/GW2Spidy/Util/Functions.php
@@ -8,9 +8,16 @@ abstract class Functions {
     }
 
     public static function cleanUpStringForCompare($string) {
+        $string = strtolower($string);
         $string = str_replace(" ", "", $string);
         $string = str_replace("'s", "", $string);
-        $string = strtolower($string);
+
+        $synonyms = array(
+            'crawfish' => 'crawdad',
+            'rough'    => 'copper',
+        );
+
+        $string = str_replace(array_keys($synonyms), array_values($synonyms), $string);
 
         return $string;
     }
