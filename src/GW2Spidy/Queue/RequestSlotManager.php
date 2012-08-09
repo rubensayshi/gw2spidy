b248d9db33e0d2de734a95d94e46df620153d67c
rubensayshi <ruben@rubensayshi.com>
2012-08-09 19:25:52 +0200

    increased the slots

--- src/GW2Spidy/Queue/RequestSlotManager.php
+++ src/GW2Spidy/Queue/RequestSlotManager.php
@@ -9,14 +9,14 @@ class RequestSlotManager extends RedisSlotManager {
         return 'request.slots';
     }
     /**
-     * 700 requests with a (250 sec = ) 4.666 min timeout
-     *  gives us 700 x (60 / (250 / 60)) = 10080 requests / hr = 2.8 requests / sec
+     * 1400 requests with a (250 sec = ) 4.666 min timeout
+     *  gives us 1400 x (60 / (250 / 60)) = 201600 requests / hr = 2.8 requests / sec
      * this is excluding the time it takes to handle the slots
      *
-     * in 1 hour we create about 6500 jobs, our total requests / hr should exceed this by at least 25% to be able to catch up
+     * in 1 round fill-queue-hourly we create about 6500 jobs
      */
     protected function getSlots() {
-        return 700;
+        return 1400;
     }
 
     protected function getTimeout() {
