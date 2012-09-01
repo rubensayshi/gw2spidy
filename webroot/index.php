5eaa8284e348e446a3e5ab3d7b8386c222475ee8
rubensayshi <ruben@rubensayshi.com>
2012-09-01 20:22:46 +0200

    hmpf

--- webroot/index.php
+++ webroot/index.php
@@ -303,8 +303,7 @@ $app->get("/api/{format}/{secret}", function($format, $secret) use($app) {
         return $app->redirect("/");
     }
 
-    ini_set('memory_limit', '512M');
-
+    Propel::disableInstancePooling();
     $items = ItemQuery::create()->find();
 
     if ($format == 'csv') {
