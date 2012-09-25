47251d6dca6a5e177acc126229a97d95ce01b0d2
Ruben de Vries <ruben@rubensayshi.com>
2012-09-25 10:57:54 +0200

    added slugify filter

--- src/GW2Spidy/Twig/GenericHelpersExtension.php
+++ src/GW2Spidy/Twig/GenericHelpersExtension.php
@@ -8,7 +8,22 @@ class GenericHelpersExtension extends \Twig_Extension {
             'ceil' => new \Twig_Filter_Method($this, 'ceil'),
             'floor' => new \Twig_Filter_Method($this, 'floor'),
             'rarity_css_class' => new \Twig_Filter_Method($this, 'rarity_css_class'),
+            'slugify' => new \Twig_Filter_Method($this, 'slugify'),
         );
+    }
+
+    public function slugify($str) {
+        $str = preg_replace('/^\s+|\s+$/', '', $str);
+        $str = strtolower($str);
+
+        $str = str_replace(str_split('ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;'), str_split('aaaaaeeeeeiiiiooooouuuunc------'), $str);
+
+        $str = preg_replace('/[^a-z0-9 -]/', '', $str);
+        $str = preg_replace('/\s+/', '-', $str);
+        $str = preg_replace('/-+/', '-', $str);
+        $str = preg_replace('/-+$/', '', $str);
+
+        return $str;
     }
 
     public function floor($num) {
