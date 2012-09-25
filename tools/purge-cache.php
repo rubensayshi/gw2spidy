80f7771b19d835be2171e6404b47c4030af91476
rubensayshi <ruben@rubensayshi.com>
2012-09-25 22:32:10 +0200

    renamed and added apc clear

--- tools/purge-cache.php
+++ tools/purge-cache.php
@@ -6,3 +6,7 @@ require dirname(__FILE__) . '/../config/config.inc.php';
 require dirname(__FILE__) . '/../autoload.php';
 
 CacheHandler::getInstance("purge")->purge();
+
+if (function_exists('apc_clear_cache')) {
+    apc_clear_cache();
+}
