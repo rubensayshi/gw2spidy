751a3b8b25d331a4d031327409a52ee5580f8443
Ruben de Vries <ruben@rubensayshi.com>
2012-09-27 11:56:37 +0200

    init

--- src/GW2Spidy/Twig/GW2MoneyExtension.php
+++ src/GW2Spidy/Twig/GW2MoneyExtension.php
@@ -13,20 +13,38 @@ class GW2MoneyExtension extends \Twig_Extension {
     }
 
     public function getGW2Money($copper) {
-        $copper = intval($copper);
+        $copper   = intval($copper);
+        if ($negative = $copper < 0) {
+            $copper *= -1;
+        }
+
         $result = "";
 
         if ($gold = floor($copper / 10000)) {
             $copper = $copper % ($gold * 10000);
+
+            if ($negative) {
+                $gold *= -1;
+            }
+
             $result .= "{$gold}g ";
         }
 
         if ($silver = floor($copper / 100)) {
             $copper = $copper % ($silver * 100);
+
+            if ($negative) {
+                $silver *= -1;
+            }
+
             $result .= "{$silver}s ";
         }
 
         if ($copper) {
+            if ($negative) {
+                $copper *= -1;
+            }
+
             $result .= "{$copper}c ";
         }
 
