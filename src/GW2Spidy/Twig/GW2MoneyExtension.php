c6d7f863fc347fa799813a859baadee662356693
rubensayshi <ruben@rubensayshi.com>
2012-10-02 22:01:30 +0200

    versioned asset urls for money images

--- src/GW2Spidy/Twig/GW2MoneyExtension.php
+++ src/GW2Spidy/Twig/GW2MoneyExtension.php
@@ -6,18 +6,33 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 class GW2MoneyExtension extends \Twig_Extension {
-        public function getFilters() {
+    protected $env;
+    protected $assetRouting;
+
+    public function getFilters() {
         return array(
             'gw2money' => new \Twig_Filter_Method($this, 'getGW2Money' ,  array('is_safe' => array('html'))),
         );
     }
 
+    public function initRuntime(\Twig_Environment $env) {
+        $this->env = $env;
+        $this->assetRouting = $this->env->getExtension('asset_routing');
+    }
+
+    public function getMoneyImagePath($path) {
+        if ($this->assetRouting) {
+            $path = $this->assetRouting->getAssetPath($path);
+        }
+
+        return $path;
+    }
+
     public function getGW2Money($copper) {
-        
-        $goldImg   = '<img src="/assets/img/gold.png" /> ';
-        $silverImg = '<img src="/assets/img/silver.png" /> ';
-        $copperImg = '<img src="/assets/img/copper.png" /> ';
-        
+        $goldImg   = '<img src="' . $this->getMoneyImagePath('/assets/img/gold.png') . '" /> ';
+        $silverImg = '<img src="' . $this->getMoneyImagePath('/assets/img/silver.png') . '" /> ';
+        $copperImg = '<img src="' . $this->getMoneyImagePath('/assets/img/copper.png') . '" /> ';
+
         $copper = intval($copper);
         $result = "";
 
