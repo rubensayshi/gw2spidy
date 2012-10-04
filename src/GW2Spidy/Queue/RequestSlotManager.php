ca9bd46cdfc0eac52238d2ad2da0ab06255c40c5
Ruben de Vries <ruben@rubensayshi.com>
2012-10-04 16:17:00 -0500

    less bursty slots

--- src/GW2Spidy/Queue/RequestSlotManager.php
+++ src/GW2Spidy/Queue/RequestSlotManager.php
@@ -9,16 +9,16 @@ class RequestSlotManager extends RedisSlotManager {
         return 'request.slots';
     }
     /**
-     *  gives us 1000 x (60 / (100 / 60)) = 36000 requests / hr = 10 requests / sec
+     *  gives us 100 x (60 / (10 / 60)) = 36000 requests / hr = 10 requests / sec
      * this is excluding the time it takes to handle the slots
      *
      */
     protected function getSlots() {
-        return 1000;
+        return 100;
     }
 
     protected function getTimeout() {
-        return 100;
+        return 10;
     }
 
     /**
