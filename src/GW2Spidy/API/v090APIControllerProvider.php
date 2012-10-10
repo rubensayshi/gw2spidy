aa5a97e89550d314a7f57535da8fa37a0b4ff134
Ruben de Vries <ruben@rubensayshi.com>
2012-10-10 14:42:15 -0500

    old api in new structure

--- src/GW2Spidy/API/v090APIControllerProvider.php
+++ src/GW2Spidy/API/v090APIControllerProvider.php
@@ -7,6 +7,10 @@ use Silex\ControllerProviderInterface;
 
 class v090APIControllerProvider extends BaseAPIControllerProvider {
     public function connect(Application $app) {
+        $toInt = function($val) {
+            return (int) $val;
+        };
+
         $controllers = $app['controllers_factory'];
 
         $controllers->get('/types', function (Application $app) {
