8b57bdb1543c62cf805f79c6cf15d2a24d85a2ac
Ruben de Vries <ruben@rubensayshi.com>
2012-10-12 16:13:33 +0200

    helper tool

---
+++ tools/item-listing-db.php
@@ -0,0 +1,35 @@
+<?php
+
+use GW2Spidy\DB\ItemQuery;
+
+use GW2Spidy\NewQueue\ItemListingDBQueueItem;
+
+/**
+ * process queue items
+ */
+
+use GW2Spidy\GW2SessionManager;
+
+use GW2Spidy\TradingPostSpider;
+
+use GW2Spidy\NewQueue\ItemDBQueueWorker;
+use GW2Spidy\NewQueue\RequestSlotManager;
+use GW2Spidy\NewQueue\ItemListingDBQueueManager;
+use GW2Spidy\NewQueue\ItemListingDBQueueWorker;
+
+
+require dirname(__FILE__) . '/../autoload.php';
+
+$queueManager = new ItemListingDBQueueManager();
+$queueWorker  = new ItemListingDBQueueWorker($queueManager);
+
+$item = ItemQuery::create()->findPk($argv[1]);
+
+if (!$item) die("failed to find item");
+
+var_dump($item->getName());
+
+$queueItem = new ItemListingDBQueueItem($item);
+var_dump($queueItem->getItemPriority());
+
+$queueWorker->work($queueItem);
