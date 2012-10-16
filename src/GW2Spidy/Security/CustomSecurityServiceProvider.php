3b9b0ef4724132596ee5fbc66178d8f3adc51074
Ruben de Vries <ruben@rubensayshi.com>
2012-10-16 14:14:11 +0200

    moved all the security stuff into our own custom provider

---
+++ src/GW2Spidy/Security/CustomSecurityServiceProvider.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace GW2Spidy\Security;
+
+use Silex\Provider\SecurityServiceProvider;
+
+use Silex\Application;
+
+use Silex\ServiceProviderInterface;
+use GW2Spidy\Security\CustomAuthenticationSuccessHandler;
+use GW2Spidy\Security\CustomLogoutSuccessHandler;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bridge\Twig\Extension\SecurityExtension;
+
+use GW2Spidy\DB\User;
+use GW2Spidy\UserProvider;
+
+class CustomSecurityServiceProvider implements ServiceProviderInterface {
+    public function register(Application $app) {
+        // register security provider
+        $app->register(new SecurityServiceProvider(), array(
+            'security.firewalls' => array(
+                    'login' => array(
+                            'pattern' => '^/login$',
+                    ),
+                    'main' => array(
+                            'anonymous' => true,
+                            'form'      => array('login_path' => '/login', 'check_path' => '/login_check'),
+                            'logout'    => array('logout_path' => '/logout'),
+                            'users'     => $app->share(function () use ($app) {
+                                return new UserProvider();
+                            }),
+                            /*'users'     => array(
+                             // raw password is foo
+                                    'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+                                    'user'  => array('ROLE_USER',  '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+                            ),*/
+                    ),
+            ),
+            'security.role_hierarchy' => array(
+                    'ROLE_ADMIN' => array('ROLE_USER', 'ROLE_ALLOWED_TO_SWITCH'),
+                    'ROLE_USER' => array(),
+            ),
+            'security.access_rules' => array(
+                    array('^/admin', 'ROLE_ADMIN'),
+            ),
+        ));
+
+        $app['security.authentication.success_handler._proto'] = $app->protect(function ($name, $options) use ($app) {
+            return $app->share(function () use ($name, $options, $app) {
+                $handler = new CustomAuthenticationSuccessHandler(
+                        $app['security.http_utils'],
+                        $options
+                );
+                $handler->setProviderKey($name);
+
+                return $handler;
+            });
+        });
+
+        $app['security.authentication.logout_handler._proto'] = $app->protect(function ($name, $options) use ($app) {
+            return $app->share(function () use ($name, $options, $app) {
+                return new CustomLogoutSuccessHandler(
+                        $app['security.http_utils'],
+                        isset($options['target_url']) ? $options['target_url'] : '/'
+                );
+            });
+        });
+
+        // hit the security.firewall_map and the security so they initialize properly before Twig tries to use them in some odd way
+        $app['security.firewall_map'];
+        $app['security'];
+
+        $app['user'] = $app->share(function() use ($app) {
+            if (!$app['security']->getToken() || !($user = $app['security']->getToken()->getUser()) || !$user instanceof User) {
+                return null;
+            }
+
+            return $user;
+        });
+
+        $app->before(function(Request $request) use ($app) {
+            $app['isLoggedIn'] = (boolean)$request->cookies->get('logged_in', null);
+
+            if (!$request->isXmlHttpRequest() && $request->getMethod() == 'GET' && !preg_match("/^\/login/", $request->getRequestUri())) {
+                $app['session']->set('_security.main.target_path', $request->getRequestUri());
+            }
+        });
+    }
+
+    public function boot(Application $app) {}
+
+}
--- webroot/index.php
+++ webroot/index.php
@@ -1,12 +1,4 @@
 <?php
-use GW2Spidy\Security\CustomAuthenticationSuccessHandler;
-use GW2Spidy\Security\CustomLogoutSuccessHandler;
-
-use Symfony\Component\HttpFoundation\Request;
-
-use Symfony\Bridge\Twig\Extension\SecurityExtension;
-
-use GW2Spidy\DB\User;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
@@ -18,9 +10,10 @@ ini_set('display_errors', 'On');
 use GW2Spidy\Util\Functions;
 
 use GW2Spidy\Application;
+use GW2Spidy\Security\CustomSecurityServiceProvider;
 
-use GW2Spidy\UserProvider;
 
+use Symfony\Component\HttpFoundation\Request;
 use GW2Spidy\Twig\VersionedAssetsRoutingExtension;
 use GW2Spidy\Twig\ItemListRoutingExtension;
 use GW2Spidy\Twig\GW2MoneyExtension;
@@ -37,67 +30,9 @@ $app->register(new Igorw\Silex\ConfigServiceProvider(getAppConfig()));
 // setup dev mode related stuff based on config
 $app['sql_logging'] && $app->enableSQLLogging();
 
-// register security provider
-$app->register(new Silex\Provider\SecurityServiceProvider(), array(
-    'security.firewalls' => array(
-        'login' => array(
-            'pattern' => '^/login$',
-        ),
-        'main' => array(
-            'anonymous' => true,
-            'form'      => array('login_path' => '/login', 'check_path' => '/login_check'),
-            'logout'    => array('logout_path' => '/logout'),
-            'users'     => $app->share(function () use ($app) {
-                return new UserProvider();
-            }),
-            /*'users'     => array(
-             // raw password is foo
-                    'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
-                    'user'  => array('ROLE_USER',  '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
-            ),*/
-        ),
-    ),
-    'security.role_hierarchy' => array(
-        'ROLE_ADMIN' => array('ROLE_USER', 'ROLE_ALLOWED_TO_SWITCH'),
-        'ROLE_USER' => array(),
-    ),
-    'security.access_rules' => array(
-        array('^/admin', 'ROLE_ADMIN'),
-    ),
-));
+// register our custom security provider
+$app->register(new CustomSecurityServiceProvider());
 
-$app['security.authentication.success_handler._proto'] = $app->protect(function ($name, $options) use ($app) {
-    return $app->share(function () use ($name, $options, $app) {
-        $handler = new CustomAuthenticationSuccessHandler(
-                $app['security.http_utils'],
-                $options
-        );
-        $handler->setProviderKey($name);
-
-        return $handler;
-    });
-});
-
-$app['security.authentication.logout_handler._proto'] = $app->protect(function ($name, $options) use ($app) {
-    return $app->share(function () use ($name, $options, $app) {
-        return new CustomLogoutSuccessHandler(
-            $app['security.http_utils'],
-            isset($options['target_url']) ? $options['target_url'] : '/'
-        );
-    });
-});
-
-// hit the security.firewall_map and the security so they initialize properly before Twig tries to use them in some odd way
-$app['security.firewall_map'];
-$app['security'];
-
-$app['user'] = $app->share(function() use ($app) {
-    if (!$app['security']->getToken() || !($user = $app['security']->getToken()->getUser()) || !$user instanceof User) {
-        return null;
-    }
-
-    return $user;
-});
 // register providers
 $app->register(new Silex\Provider\SessionServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
@@ -113,14 +48,6 @@ $app['twig']->addExtension(new GenericHelpersExtension());
 $app['twig']->addExtension(new VersionedAssetsRoutingExtension());
 $app['twig']->addExtension(new GW2MoneyExtension());
 $app['twig']->addExtension(new ItemListRoutingExtension($app['url_generator']));
-
-$app->before(function(Request $request) use ($app) {
-    $app['isLoggedIn'] = (boolean)$request->cookies->get('logged_in', null);
-
-    if (!$request->isXmlHttpRequest() && $request->getMethod() == 'GET' && !preg_match("/^\/login/", $request->getRequestUri())) {
-        $app['session']->set('_security.main.target_path', $request->getRequestUri());
-    }
-});
 
 /*
  * it's not very clean and silex-like but following are some includes to split up all the routing / functionality
