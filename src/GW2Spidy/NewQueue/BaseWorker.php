d93045b6fd4278846762f969527e8569f8ad2195
Ruben de Vries <ruben@rubensayshi.com>
2012-10-17 15:17:35 +0200

    fixed not properly storing listings with 0 avail

--- src/GW2Spidy/NewQueue/BaseWorker.php
+++ src/GW2Spidy/NewQueue/BaseWorker.php
@@ -33,34 +33,49 @@ class BaseWorker {
         $now  = new DateTime();
         $item = $item ?: ItemQuery::create()->findPK($itemData['data_id']);
 
-        $item->setOfferAvailability($itemData['sale_availability']);
-        if (isset($itemData['min_sale_unit_price']) && $itemData['min_sale_unit_price'] > 0) {
-            $item->setMinSaleUnitPrice($itemData['min_sale_unit_price']);
+        // ensure the expected keys are there
+        $itemData['sale_availability']    = isset($itemData['sale_availability'])    ? $itemData['sale_availability']    : 0;
+        $itemData['min_sale_unit_price']  = isset($itemData['min_sale_unit_price'])  ? $itemData['min_sale_unit_price']  : 0;
+        $itemData['offer_availability']   = isset($itemData['offer_availability'])   ? $itemData['offer_availability']   : 0;
+        $itemData['max_offer_unit_price'] = isset($itemData['max_offer_unit_price']) ? $itemData['max_offer_unit_price'] : 0;
 
-            $sellListing = new SellListing();
-            $sellListing->setItem($item);
-            $sellListing->setListingDatetime($now);
-            $sellListing->setQuantity($itemData['sale_availability'] ?: 0);
+        // sale
+        $item->setSaleAvailability($itemData['sale_availability']);
+        $sellListing = new SellListing();
+        $sellListing->setItem($item);
+        $sellListing->setListingDatetime($now);
+
+        if ($itemData['sale_availability'] == 0 || $itemData['min_sale_unit_price'] == 0) {
+            $sellListing->setQuantity(0);
+            $sellListing->setUnitPrice($item->getMinSaleUnitPrice());
+            $sellListing->setListings(0);
+        } else {
+            $item->setMinSaleUnitPrice($itemData['min_sale_unit_price']);
+            $sellListing->setQuantity($itemData['sale_availability']);
             $sellListing->setUnitPrice($itemData['min_sale_unit_price']);
             $sellListing->setListings(1);
-
-            $sellListing->save();
         }
+        $sellListing->save();
 
+        // offer
         $item->setOfferAvailability($itemData['offer_availability']);
-        if (isset($itemData['max_offer_unit_price']) && $itemData['max_offer_unit_price'] > 0) {
-            $item->setMaxOfferUnitPrice($itemData['max_offer_unit_price']);
+        $buyListing = new BuyListing();
+        $buyListing->setItem($item);
+        $buyListing->setListingDatetime($now);
 
-            $buyListing = new BuyListing();
-            $buyListing->setItem($item);
-            $buyListing->setListingDatetime($now);
-            $buyListing->setQuantity($itemData['offer_availability'] ?: 0);
+        if ($itemData['offer_availability'] == 0 || $itemData['max_offer_unit_price'] == 0) {
+            $buyListing->setQuantity(0);
+            $buyListing->setUnitPrice($item->getMaxOfferUnitPrice());
+            $buyListing->setListings(0);
+        } else {
+            $item->setMaxOfferUnitPrice($itemData['max_offer_unit_price']);
+            $buyListing->setQuantity($itemData['offer_availability']);
             $buyListing->setUnitPrice($itemData['max_offer_unit_price']);
             $buyListing->setListings(1);
-
-            $buyListing->save();
         }
+        $buyListing->save();
 
+        // save if not done externally
         if ($save) {
             $item->save();
         }
