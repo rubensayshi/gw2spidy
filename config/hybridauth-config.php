009d513bc3868e0c5376ab1696aadbda9ec58ef3
Ruben de Vries <ruben@rubensayshi.com>
2012-11-24 12:58:26 -0600

    less rights on google

--- config/hybridauth-config.php
+++ config/hybridauth-config.php
@@ -7,67 +7,70 @@
 */
 
 // ----------------------------------------------------------------------------------------
-//	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
+//    HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
 // ----------------------------------------------------------------------------------------
 
 return
-	array(
-		"base_url" => getAppConfig("hybrid_auth.callback_url"),
+    array(
+        "base_url" => getAppConfig("hybrid_auth.callback_url"),
 
-		"providers" => array (
-			// openid providers
-			"OpenID" => array (
-				"enabled" => false
-			),
+        "providers" => array (
+            // openid providers
+            "OpenID" => array (
+                "enabled" => false
+            ),
 
-			"AOL"  => array (
-				"enabled" => false
-			),
+            "AOL"  => array (
+                "enabled" => false
+            ),
 
-			"Yahoo" => array (
-				"enabled" => false,
-				"keys"    => array ( "id" => "", "secret" => "" )
-			),
+            "Yahoo" => array (
+                "enabled" => false,
+                "keys"    => array ( "id" => "", "secret" => "" )
+            ),
 
-			"Google" => array (
-				"enabled" => true,
-				"keys"    => array ( "id" => getAppConfig("hybrid_auth.google_id"), "secret" => getAppConfig("hybrid_auth.google_secret"))
-			),
+            "Google" => array (
+                "enabled" => true,
+                "keys"    => array ( "id" => getAppConfig("hybrid_auth.google_id"), "secret" => getAppConfig("hybrid_auth.google_secret")),
+                "scope"           => implode(" ", array("https://www.googleapis.com/auth/userinfo.profile",
+                                                        "https://www.googleapis.com/auth/userinfo.email")),
+                "access_type"     => "online",
+            ),
 
-			"Facebook" => array (
-				"enabled" => true,
-				"keys"    => array ( "id" => getAppConfig("hybrid_auth.facebook_id"), "secret" => getAppConfig("hybrid_auth.facebook_secret"))
-			),
+            "Facebook" => array (
+                "enabled" => true,
+                "keys"    => array ( "id" => getAppConfig("hybrid_auth.facebook_id"), "secret" => getAppConfig("hybrid_auth.facebook_secret"))
+            ),
 
-			"Twitter" => array (
-				"enabled" => true,
-				"keys"    => array ( "key" => getAppConfig("hybrid_auth.twitter_key"), "secret" => getAppConfig("hybrid_auth.twitter_secret"))
-			),
+            "Twitter" => array (
+                "enabled" => true,
+                "keys"    => array ( "key" => getAppConfig("hybrid_auth.twitter_key"), "secret" => getAppConfig("hybrid_auth.twitter_secret"))
+            ),
 
-			// windows live
-			"Live" => array (
-				"enabled" => false,
-				"keys"    => array ( "id" => "", "secret" => "" )
-			),
+            // windows live
+            "Live" => array (
+                "enabled" => false,
+                "keys"    => array ( "id" => "", "secret" => "" )
+            ),
 
-			"MySpace" => array (
-				"enabled" => false,
-				"keys"    => array ( "key" => "", "secret" => "" )
-			),
+            "MySpace" => array (
+                "enabled" => false,
+                "keys"    => array ( "key" => "", "secret" => "" )
+            ),
 
-			"LinkedIn" => array (
-				"enabled" => false,
-				"keys"    => array ( "key" => "", "secret" => "" )
-			),
+            "LinkedIn" => array (
+                "enabled" => false,
+                "keys"    => array ( "key" => "", "secret" => "" )
+            ),
 
-			"Foursquare" => array (
-				"enabled" => false,
-				"keys"    => array ( "id" => "", "secret" => "" )
-			),
-		),
+            "Foursquare" => array (
+                "enabled" => false,
+                "keys"    => array ( "id" => "", "secret" => "" )
+            ),
+        ),
 
-		// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
-		"debug_mode" => false,
+        // if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
+        "debug_mode" => false,
 
-		"debug_file" => "/tmp/hauth.log"
-	);
+        "debug_file" => "/tmp/hauth.log"
+    );
