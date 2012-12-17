4473df83960c01d1b0f57cdab239f2c0d4516d64
Ruben de Vries <ruben@rubensayshi.com>
2012-12-17 14:13:48 -0600

    no gzip

--- src/GW2Spidy/Dataset/DatasetManager.php
+++ src/GW2Spidy/Dataset/DatasetManager.php
@@ -12,7 +12,7 @@ class DatasetManager extends Singleton {
     protected $useCache = true;
 
     public function __construct() {
-        $this->cache = RedisCacheHandler::getInstance('datasets', true,  10800, true);
+        $this->cache = RedisCacheHandler::getInstance('datasets', true,  10800);
     }
 
     public function setUseCache($useCache = true) {
