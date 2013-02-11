6efd56fbc327e8f2d0cc4091c16e88dbb2c0f424
Ruben de Vries <ruben@rubensayshi.com>
2013-02-11 07:37:56 -0600

    updated sql

--- config/migrations/20121125.manual.vendorprices.sql
+++ config/migrations/20121125.manual.vendorprices.sql
@@ -27,26 +27,6 @@ UPDATE item SET vendor_price = 8  WHERE name LIKE 'Bag of Starch';
 UPDATE item SET vendor_price = 8  WHERE name LIKE 'Bag of Flour';
 UPDATE item SET vendor_price = 8  WHERE name LIKE 'Bottle of Soy Sauce';
 
-UPDATE gw2db_item_archive LEFT JOIN item
-     ON gw2db_item_archive.DataID = item.data_id
-    SET gw2db_item_archive.karma_price = 2,
-        item.karma_price = 2
-  WHERE gw2db_item_archive.Name IN ('Packet of Yeast','Packet[s] of Yeast');
-UPDATE gw2db_item_archive LEFT JOIN item
-     ON gw2db_item_archive.DataID = item.data_id
-    SET gw2db_item_archive.karma_price = 3,
-        item.karma_price = 3
-  WHERE gw2db_item_archive.Name IN ('Bowl of Sour Cream','Bowl[s] of Sour Cream');
-UPDATE gw2db_item_archive LEFT JOIN item
-     ON gw2db_item_archive.DataID = item.data_id
-    SET gw2db_item_archive.karma_price = 8,
-        item.karma_price = 8
-  WHERE gw2db_item_archive.Name IN ('Mango','Mango[s]');
-  
--- automatically set by new script: UPDATE item SET karma_price = 2 WHERE name LIKE 'Cheese Wedge';
--- automatically set by new script: UPDATE item SET karma_price = 2 WHERE name LIKE 'Glass of Buttermilk';
--- automatically set by new script: UPDATE item SET karma_price = 2 WHERE name LIKE 'Rice Ball';
-
--- incorrect: UPDATE item SET karma_price = 1000  WHERE name LIKE "Adept's Training Manual";
--- incorrect: UPDATE item SET karma_price = 10000 WHERE name LIKE "Master's Training Manual";
--- incorrect: UPDATE item SET karma_price = 20000 WHERE name LIKE "Grandmaster's Training Manual";
+UPDATE item SET item.karma_price = 2 WHERE name IN ('Packet of Yeast', 'Packet[s] of Yeast');
+UPDATE item SET item.karma_price = 3 WHERE name IN ('Bowl of Sour Cream','Bowl[s] of Sour Cream');
+UPDATE item SET item.karma_price = 8 WHERE name IN ('Mango','Mango[s]');
