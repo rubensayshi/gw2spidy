6febc1bd7bd3fae054635b077518442ea29769c3
Ryan Myers <ryan.p.myers@gmail.com>
2013-09-07 10:48:46 -0400

    ItemTypes and ItemSubTypes are now properly created/stored for each item using the update-items-from-api tool. Also fixed really minor bug in the css. (extra comma)

--- tools/update-items-from-api.php
+++ tools/update-items-from-api.php
@@ -4,12 +4,15 @@ use GW2Spidy\TradingPostSpider;
 use GW2Spidy\DB\ItemQuery;
 use GW2Spidy\DB\Item;
 use GW2Spidy\DB\ItemTypeQuery;
+use GW2Spidy\DB\ItemSubTypeQuery;
+use GW2Spidy\DB\ItemSubType;
 
 ini_set('memory_limit', '1G');
 
 require dirname(__FILE__) . '/../autoload.php';
 
 function getIDFromMarketData ($marketData, $searchValue) {
+    //Replace left value with right value
     $keys = array(
         'Crafting Material'     => 'CraftingMaterial',
         'Crafting Component'    => 'CraftingComponent',
@@ -34,6 +37,23 @@ function getIDFromMarketData ($marketData, $searchValue) {
     return $marketID;
 }
 
+function getSubIDFromMarketData ($marketData, $searchValue, $itemSubTypeName) {    
+    $marketID = null; //Default to null if it doesn't exist.
+    
+    foreach ($marketData as $marketValues) {
+        if ($marketValues['name'] == $searchValue) {
+            foreach ($marketValues as $subs) {
+                if (isset($subs['name']) && $subs['name'] == $itemSubTypeName) {
+                    $marketID = $subs['id'];
+                    break;
+                }
+            }
+        }
+    }
+    
+    return $marketID;
+}
+
 $market_data = TradingPostSpider::getInstance()->getMarketData();
 
 $curl = CurlRequest::newInstance(getAppConfig('gw2spidy.gw2api_url')."/v1/items.json") ->exec();
@@ -46,7 +66,7 @@ $render_url = getAppConfig('gw2spidy.gw2render_url')."/file";
 $number_of_items = count($data['items']) - 1;
 $item_start = (isset($argv[1]) && $argv[1] >= 1)                ? $argv[1] - 1 : 0;    //Default the start at zero.
 $item_end   = (isset($argv[2]) && $argv[2] <= $number_of_items) ? $argv[2] - 1 : $number_of_items - 1; //Max items default
-
+$itemSubTypes = array();
 for ($i = $item_start; $i <= $item_end; $i++) {
     try {
         echo "[{$i} / {$item_end}]: ";
@@ -59,7 +79,6 @@ for ($i = $item_start; $i <= $item_end; $i++) {
         $itemData = array(  'type_id'           => getIDFromMarketData($market_data['types'], $API_item['type']),
                             'data_id'           => $data['items'][$i],
                             'name'              => $API_item['name'],
-                            //'gem_store_description'       => $item['description'],
                             'restriction_level' => $API_item['level'],
                             'rarity'            => getIDFromMarketData($market_data['rarities'], $API_item['rarity']),
                             'vendor_sell_price' => $API_item['vendor_value'],
@@ -70,21 +89,76 @@ for ($i = $item_start; $i <= $item_end; $i++) {
         
         if ($item === null) {
             $item = new Item();
-        }        
+        }
         
         $item->fromArray($itemData, \BasePeer::TYPE_FIELDNAME);
-        /*
+        
         $itemType = ItemTypeQuery::create()->findPk($itemData['type_id']);
         
         if ($itemType !== null) {
-            if (isset($API_item[strtolower($API_item['type'])])) {
-                print_r($API_item[strtolower($API_item['type'])]) . "\n";
+            //Known item types with no subtypes.
+            $noSubTypes = array("CraftingMaterial", "Trophy", "MiniPet", "Bag", "Back");
+            
+            if (!in_array($API_item['type'], $noSubTypes)) {
+                $itemTypeName = strtolower($API_item['type']);
+                
+                //Workaround for upgradecomponents
+                if ($itemTypeName == 'upgradecomponent') {
+                    $itemTypeName = 'upgrade_component';
+                }
+                
+                $itemSubTypeName = $API_item[$itemTypeName]['type'];
+                
+                //Replace left value with right value
+                $keys = array(
+                    'Harpoon Gun'   => 'Harpoon',
+                    'Aquatic Helm'  => 'HelmAquatic',
+                    'Spear'         => 'Speargun',
+                    'Short Bow'     => 'ShortBow',
+                    'Gift Box'      => 'GiftBox'
+                );
+
+                if (in_array($itemSubTypeName, $keys)) {
+                    $a = array_keys($keys, $itemSubTypeName);
+                    $itemSubTypeName = $a[0];
+                }
+                
+                $itemSubType = ItemSubTypeQuery::create()->findOneByTitle($itemSubTypeName);
+                
+                if ($itemSubType !== null) {
+                    $itemType->addSubType($itemSubType);
+                    $item->setItemSubType($itemSubType);
+                }
+                else {
+                    //All of the below types are known to not exist in the market data with an ID (by this name).
+                    //Rune/Sigil/Utility/Gem/Booze/Halloween/LargeBundle/RentableContractNpc/ContractNPC/UnlimitedConsumable
+                    //TwoHandedToy/AppearanceChange/Immediate/Unknown
+                    
+                    $SubTypeID = getSubIDFromMarketData($market_data['types'], $API_item['type'], $itemSubTypeName);
+                    
+                    //If the SubTypeID cannot be found in the market data, then just create it by adding one to the highest
+                    //Subtype id within the current ItemType.
+                    if ($SubTypeID === null) {
+                        $itemSubTypes = ItemSubTypeQuery::create()
+                                ->filterByMainTypeId($itemData['type_id'])
+                                ->withColumn('MAX(id)', 'MAXid')
+                                ->find();
+                        
+                        $SubTypeID = $itemSubTypes[0]->getMAXid() + 1;
+                    }
+                    
+                    $itemSubType = new ItemSubType();
+                    $itemSubType->fromArray(array('Id' => $SubTypeID, 'MainTypeId' => $itemData['type_id'], 'Title' => $itemSubTypeName));
+                    $itemSubType->save();
+                    
+                    $itemType->addSubType($itemSubType);
+                    $item->setItemSubType($itemSubType);
+                }
             }
-            else {
-                print_r($API_item);
-            }
+            
+            $item->setItemType($itemType);
         }
-        */
+        
         $item->save();
     } catch (Exception $e) {
         $error_values[] = $data['items'][$i];
