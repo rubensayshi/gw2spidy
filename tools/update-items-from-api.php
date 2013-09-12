d4dd0fc7871ab30e7b9a2f90aff1e5fd31bef1e8
Ruben de Vries <ruben@rubensayshi.com>
2013-09-12 16:59:44 +0200

    close curl handlers

--- tools/update-items-from-api.php
+++ tools/update-items-from-api.php
@@ -19,27 +19,27 @@ function getIDFromMarketData ($marketData, $searchValue) {
         'Upgrade Component'     => 'UpgradeComponent',
         'Mini'                  => 'MiniPet'
     );
-    
+
     if (in_array($searchValue, $keys)) {
         $a = array_keys($keys, $searchValue);
         $searchValue = $a[0];
     }
-    
+
     $marketID = 0;
-    
+
     foreach ($marketData as $marketValues) {
         if ($marketValues['name'] == $searchValue) {
             $marketID = $marketValues['id'];
             break;
         }
     }
-    
+
     return $marketID;
 }
 
-function getSubIDFromMarketData ($marketData, $searchValue, $itemSubTypeName) {    
+function getSubIDFromMarketData ($marketData, $searchValue, $itemSubTypeName) {
     $marketID = null; //Default to null if it doesn't exist.
-    
+
     foreach ($marketData as $marketValues) {
         if ($marketValues['name'] == $searchValue) {
             foreach ($marketValues as $subs) {
@@ -50,7 +50,7 @@ function getSubIDFromMarketData ($marketData, $searchValue, $itemSubTypeName) {
             }
         }
     }
-    
+
     return $marketID;
 }
 
@@ -75,19 +75,23 @@ for ($i = $item_start; $i <= $item_end; $i++) {
     $ch = curl_init(getAppConfig('gw2spidy.gw2api_url')."/v1/item_details.json?item_id={$data['items'][$i]}");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $item_curls[$i] = $multi_curl->addCurl($ch);
-    
+
     echo "[{$i} / {$item_end}]: {$data['items'][$i]}\n";
 }
 
 for ($i = $item_start; $i <= $item_end; $i++) {
     try {
         echo "[{$i} / {$item_end}]: ";
-        
-        $API_item = json_decode($item_curls[$i]->data, true);
+
+        $ch = $item_curls[$i];
+
+        $API_item = json_decode($ch->data, true);
         if (!isset($API_item['name'])) throw new Exception("Item not found: $i");
-        
+
+        curl_close($ch);
+
         echo $API_item['name'] . "\n";
-        
+
         $itemData = array(  'type_id'           => getIDFromMarketData($market_data['types'], $API_item['type']),
                             'data_id'           => $data['items'][$i],
                             'name'              => $API_item['name'],
@@ -96,31 +100,31 @@ for ($i = $item_start; $i <= $item_end; $i++) {
                             'vendor_sell_price' => $API_item['vendor_value'],
                             'img'               => "{$render_url}/{$API_item['icon_file_signature']}/{$API_item['icon_file_id']}.png",
                             'rarity_word'       => $API_item['rarity']);
-        
+
         $item = ItemQuery::create()->findPK($data['items'][$i]);
-        
+
         if ($item === null) {
             $item = new Item();
         }
-        
+
         $item->fromArray($itemData, \BasePeer::TYPE_FIELDNAME);
-        
+
         $itemType = ItemTypeQuery::create()->findPk($itemData['type_id']);
-        
+
         if ($itemType !== null) {
             //Known item types with no subtypes.
             $noSubTypes = array("CraftingMaterial", "Trophy", "MiniPet", "Bag", "Back");
-            
+
             if (!in_array($API_item['type'], $noSubTypes)) {
                 $itemTypeName = strtolower($API_item['type']);
-                
+
                 //Workaround for upgradecomponents
                 if ($itemTypeName == 'upgradecomponent') {
                     $itemTypeName = 'upgrade_component';
                 }
-                
+
                 $itemSubTypeName = $API_item[$itemTypeName]['type'];
-                
+
                 //Replace left value with right value
                 $keys = array(
                     'Harpoon Gun'   => 'Harpoon',
@@ -134,9 +138,9 @@ for ($i = $item_start; $i <= $item_end; $i++) {
                     $a = array_keys($keys, $itemSubTypeName);
                     $itemSubTypeName = $a[0];
                 }
-                
+
                 $itemSubType = ItemSubTypeQuery::create()->findOneByTitle($itemSubTypeName);
-                
+
                 if ($itemSubType !== null) {
                     $itemType->addSubType($itemSubType);
                     $item->setItemSubType($itemSubType);
@@ -145,9 +149,9 @@ for ($i = $item_start; $i <= $item_end; $i++) {
                     //All of the below types are known to not exist in the market data with an ID (by this name).
                     //Rune/Sigil/Utility/Gem/Booze/Halloween/LargeBundle/RentableContractNpc/ContractNPC/UnlimitedConsumable
                     //TwoHandedToy/AppearanceChange/Immediate/Unknown
-                    
+
                     $SubTypeID = getSubIDFromMarketData($market_data['types'], $API_item['type'], $itemSubTypeName);
-                    
+
                     //If the SubTypeID cannot be found in the market data, then just create it by adding one to the highest
                     //Subtype id within the current ItemType.
                     if ($SubTypeID === null) {
@@ -155,29 +159,29 @@ for ($i = $item_start; $i <= $item_end; $i++) {
                                 ->filterByMainTypeId($itemData['type_id'])
                                 ->withColumn('MAX(id)', 'MAXid')
                                 ->find();
-                        
+
                         $SubTypeID = $itemSubTypes[0]->getMAXid() + 1;
                     }
-                    
+
                     $itemSubType = new ItemSubType();
                     $itemSubType->fromArray(array('Id' => $SubTypeID, 'MainTypeId' => $itemData['type_id'], 'Title' => $itemSubTypeName));
                     $itemSubType->save();
-                    
+
                     $itemType->addSubType($itemSubType);
                     $item->setItemSubType($itemSubType);
                 }
             }
-            
+
             $item->setItemType($itemType);
         }
-        
+
         $item->save();
     } catch (Exception $e) {
         $error_values[] = $data['items'][$i];
-        
+
         echo "failed [[ {$e->getMessage()} ]] .. \n";
     }
 }
 
-if (count($error_values) > 0) 
+if (count($error_values) > 0)
     var_dump($error_values);
